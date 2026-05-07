68e5ebf422130bf93c2ee85d195552f504207424
Elysyrus <1240878885@qq.com>
2026-05-07 22:12:52 +0800

    docs: add some code comments

--- src/main.cpp
+++ src/main.cpp
@@ -9,9 +9,12 @@ void print_usage() {
     std::cout << "  <path>   Directory to analyze" << std::endl;
 }
 
+// main函数里面的参数是命令行工具的入口
+// argc 是命令行参数的数量，argv 是一个数组，包含了所有的命令行参数
 int main(int argc, char* argv[]) {
     // 把命令行参数转成 vector，更方便处理
     // argv 是 C 风格的数组，这里把它转成现代 C++ 的 vector
+    // auto是类型推断，让编译器自行推断这是什么类型
     auto args = std::vector<std::string>(argv + 1, argv + argc);
 
     if (args.empty()) {
